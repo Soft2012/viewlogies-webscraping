79b9c5bc3f52914668a3692cf4b6ff6c7c4fbf0d
Soft2012 <TeamClick2012@outlook.com>
2024-03-29 03:57:11 +0900

    complete code and running script

---
+++ run.py
@@ -0,0 +1,65 @@
+# import requests
+# from datetime import datetime
+import csv
+from selenium.webdriver.common.by import By
+import time
+from seleniumbase import Driver
+from selenium.webdriver.support.ui import Select
+# import json
+
+header = ["Logo", "Company", "SLUG", "URL", "Pin", "Service Date"]
+with open('result.csv', mode='a', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(header)
+
+driver = Driver(uc=True)
+
+pin_count = 0
+for i in range(999547, 0, -1):
+    before_code = ""
+    pin_number = pin_count + i
+    pin_initial_code = str(pin_number)
+    # print("initial code : ", pin_initial_code)
+    for j in range(6-len(pin_initial_code)):
+        before_code = before_code + "0"
+    pin_code = before_code + pin_initial_code
+    print(pin_code)
+
+    driver.get("https://www.viewlogies.net/locate")
+    input_boxs = driver.find_elements(By.CSS_SELECTOR, 'div.react-code-input > input')
+    for k in range(len(input_boxs)):
+        input_box = input_boxs[k]
+        input_box.send_keys(pin_code[k])
+
+    time.sleep(2)
+    try:
+        submit_btn = driver.find_elements(By.CLASS_NAME, 'viewlogies-button')[0]
+        submit_btn.click()
+
+        time.sleep(3)
+        try:
+            image_url = driver.find_element(By.CSS_SELECTOR, '.client-header img').get_attribute('src')
+        except:
+            image_url = ""
+        current_page = driver.current_url
+        slug = "/" + current_page.split("/")[3]
+        try:
+            company_name = driver.find_elements(By.CSS_SELECTOR, '.contact-us-content p')[1].text.split('video')[0]
+        except:
+            company_name = ""
+        try:
+            service_time = driver.find_elements(By.CSS_SELECTOR, '.subheader h3')[0].text.split(":")[1].strip()
+            service_clock = service_time.split(" ")[-1]
+            service_date = service_time.replace(service_clock, "").strip()
+        except:
+            service_date = ""
+
+        result_data = [image_url, company_name, slug, current_page, pin_code, service_date]
+        with open('result.csv', mode='a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(result_data)
+
+    except:
+        pass
+
+driver.quit()
